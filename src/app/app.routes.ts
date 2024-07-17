824c74c8c2da6b0f38380de8ab6443c60822a0f1
Nicholas.Irving <Nicholas.Irving@development.ndis.gov.au>
2024-07-18 09:56:05 +1000

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,19 @@
 import { Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { FailedComponent } from './components/failed/failed.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProfileComponent } from './components/profile/profile.component';
 import { CSRComponent } from './components/csr/csr.component';
 
 export const routes: Routes = [
-  {
-    path: 'profile',
-    component: ProfileComponent,
-    canActivate: [MsalGuard],
-  },
   {
     path: 'csr',
     component: CSRComponent,
     canActivate: [MsalGuard],
   },
-  { path: '', redirectTo: 'csr', pathMatch: 'full' },
+  { 
+    path: '', 
+    redirectTo: 'csr', 
+    pathMatch: 'full' 
+  },
   {
     path: 'login-failed',
     component: FailedComponent,
